50de21220388c0a800ef50988927335ffd3b236d
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-23 15:37:57 +0300

    accepted, but ugly, expect changes

--- w2/soup_sample/wikistat.py
+++ w2/soup_sample/wikistat.py
@@ -1,13 +1,23 @@
 from bs4 import BeautifulSoup
 import re
 import os
-
+import json
 
 # Вспомогательная функция, её наличие не обязательно и не будет проверяться
 def build_tree(start, end, path):
-    link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
-    files = dict.fromkeys(os.listdir(path))  # Словарь вида {"filename1": None, "filename2": None, ...}
-    # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
+    files = dict.fromkeys(os.listdir(path), [])
+
+    with open(path + start, 'r') as f:
+        html = f.read()
+    soup = BeautifulSoup(html, 'lxml')
+    root = soup.find('div', {'id': 'bodyContent'})
+    for a in root.findAll('a'):
+        if a.has_attr('title') and a.text in files:
+            if a['title'] not in files[start]:
+                files[start].append({'title': a['title'], 'href': a['href']})
+
+    with open('data.json', 'w') as f:
+        json.dump(files, f)
     return files
 
 
@@ -27,7 +37,7 @@ def parse(start, end, path):
     Чтобы получить максимальный балл, придется искать все страницы. Удачи!
     """
 
-    bridge = build_bridge(start, end, path)  # Искать список страниц можно как угодно, даже так: bridge = [end, start]
+    bridge = [end, start] # build_bridge(start, end, path)  # Искать список страниц можно как угодно, даже так: bridge = [end, start]
 
     # Когда есть список страниц, из них нужно вытащить данные и вернуть их
     out = {}
@@ -38,10 +48,34 @@ def parse(start, end, path):
         body = soup.find(id="bodyContent")
 
         # TODO посчитать реальные значения
-        imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
-        headers = 10  # Количество заголовков, первая буква текста внутри которого: E, T или C
-        linkslen = 15  # Длина максимальной последовательности ссылок, между которыми нет других тегов
-        lists = 20  # Количество списков, не вложенных в другие списки
+        imgs = 0  # Количество картинок (img) с шириной (width) не меньше 200
+        headers = 0  # Количество заголовков, первая буква текста внутри которого: E, T или C
+        linkslen = 0  # Длина максимальной последовательности ссылок, между которыми нет других тегов
+        lists = 0  # Количество списков, не вложенных в другие списки
+
+        for img in body.findAll('img'):
+            if img.has_attr('width'):
+                if int(img['width']) >= 200:
+                    imgs += 1
+
+        for h in body.find_all(re.compile(r'h\d+')):
+            if h.text[0] in ['E', 'T', 'C']:
+                headers += 1
+
+        current = 0
+        for a in body.findAll('a'):
+            sibling = a.find_next_sibling()
+            if sibling:
+                for s in sibling:
+                    if s.name == 'a':
+                        current += 1
+            if current > linkslen:
+                linkslen = current
+                current = 0
+
+        for el in body.find_all(['ul', 'ol']):
+            if el.parent.name not in 'li':
+                lists += 1
 
         out[file] = [imgs, headers, linkslen, lists]
 
